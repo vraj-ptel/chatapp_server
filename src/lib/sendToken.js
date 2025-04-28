b162d0160fd8a5a64fb7744d59eb3f148ad2129f
vraj <106588213+vraj-ptel@users.noreply.github.com>
2025-04-28 11:40:11 +0530

    jwt solve 2

--- src/lib/sendToken.js
+++ src/lib/sendToken.js
@@ -5,13 +5,13 @@ const sendToken = (res, user, message, statusCode) => {
   const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
     expiresIn: "5D",
   });
-  // console.log("token", token);
+  console.log("token", token);
   return res
     .status(statusCode)
     .cookie("chat2", token, {
       maxAge: 24 * 60 * 60 * 1000 * 5,
       httpOnly: true,
-      sameSite: "none",
+      sameSite: "None",
       secure: true,
     })
     .json({ success: true, message, user });
